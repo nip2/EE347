1b661ad0a63215e0dfda9e26c7d6c5629e0822bc
Nick Porter <nip2@pdx.edu>
2022-02-01 21:05:55 -0800

    added working pf correction fcn

--- lab/lab2/calculations.py
+++ lab/lab2/calculations.py
@@ -1,28 +1,21 @@
 # EE347 Lab 2
 # Team 2: 
-# 2.2 Engineering Design
+# 2.1 Calculations and verification
 
 import math
 import cmath
 
-# Design a power factor correction function that calculates required reactive power
-# for a given load according to the following specifications:
-# S_load = P_l + j*Q_l = [1 + j0:10 + j10] MVA exclusive of |S_load| > 10 MVA
-# function shall return capacitance Q_c required to maintain
-# 0.95 lagging <= PF <= 1.0 for a given S_load
-# Q_c shall be limited to discrete values in increments of 0.25 MVAr
-
-# start with analysis of lab data
+# analysis of lab data
 # Case 1
 print("\nCase 1")
 # initialize values
 R0 = 600    # ohms
-C = [-171,-200,-240,-300,-400,-600,-1200]   # j ohms
+C = [-171j,-200j,-240j,-300j,-400j,-600j,-1200j]   # ohms
 Z = []
 
-# Find total impedance, Z: R0||C = 1 / (1/R0 + 1/jC)
+# Find total impedance, Z: R0||C = 1 / (1/R0 + 1/C)
 for c in C:
-    Z.append(1/(1/R0+1/complex(0,c)))
+    Z.append(1/(1/R0+1/c))
 
 # include the C = open case in Z array:
 Z.append(R0)
@@ -36,7 +29,6 @@ for z in Z:
 print("\npolar Z:")
 for z in Z:
     zmag = abs(z)
-    #zang = cmath.phase(z)      # radians
     zang = math.degrees(cmath.phase(z))
     print(f"{zmag:.2f} ohms, angle {zang:.2f} degrees")
 
@@ -57,14 +49,13 @@ for i in I1:
 print("\nCase 2")
 # initialize values
 R0 = 300    # ohms
-L = 300     # j ohms
-Z0 = complex(R0,L)
-C = [-171,-200,-240,-300,-400,-600,-1200]   # j ohms
+L = 300j     # ohms
+Z0 = (R0+L)
 Z = []
 
-# Find total impedance, Z: Z0||C = 1 / (1/Z0 + 1/jC)
+# Find total impedance, Z: Z0||C = 1 / (1/Z0 + 1/C)
 for c in C:
-    Z.append(1/(1/Z0+1/complex(0,c)))
+    Z.append(1/(1/Z0+1/c))
 
 # include the C = open case in Z array:
 Z.append(Z0)
@@ -92,4 +83,3 @@ for i in I2:
     imag = abs(i)
     iang = math.degrees(cmath.phase(i))
     print(f"{imag:.2f} A, angle {iang:.2f} degrees")
-
---
+++ lab/lab2/pf_correct_fcn.py
@@ -0,0 +1,57 @@
+# EE347 Lab 2
+# Team 2: 
+# 2.2 Engineering Design
+
+import math
+import cmath
+import numpy as np
+
+# Design a power factor correction function that calculates required reactive power
+# for a given load according to the following specifications:
+# S_load = P_l + j*Q_l = [1 + j0:10 + j10] MVA exclusive of |S_load| > 10 MVA
+# function shall return capacitance Q_c required to maintain
+# 0.95 lagging <= PF <= 1.0 for a given S_load
+# Q_c shall be limited to discrete values in increments of 0.25 MVAr
+
+N = 50                      # NxN possible combinations
+pf_cutoff = 0.95            # pf should be at or above this value
+P = np.linspace(1,10,N)     # N evenly spaced values btwn 1 and 10
+Q = np.linspace(0,10,N)     # N evenly spaced values btwn 0 and 10
+qc_step = .25               # discrete values of Qc with this step size
+Qc = np.arange(0,-N,-qc_step)# N possible values of Qc btwn 0 and -N*qc_step
+count = 0
+
+def pf_correction(p,q):
+    global count
+    S = complex(p,q)
+    S_mag = abs(S)
+    pf = p/S_mag
+    q_target = p*math.tan(math.acos(pf_cutoff))
+
+    if pf >= pf_cutoff:
+        count = count + 1
+        print(f"P = {p:.2}, Q = {q:.2}")
+        print(f"S_mag = {S_mag:.2}     \t pf = {pf:.2}")
+
+    else:
+        count = count + 1
+        for qc in Qc:
+            q_nu = q + qc
+            if q_nu <= q_target:
+                S_mag = math.sqrt(p**2+q_nu**2)
+                pf = p/S_mag
+                        
+                print("pf correction")
+                #print(f"used Qc = {qc:.1} to correct")
+                #print(f"The following values yield a pf >= {pf_cutoff:.2}:")
+                #print(f"S = [{p:.1} + j{q_nu:.1}] MVA")
+                #print(f"|S| = {abs(S_mag):.1}")
+                print(f"PF = {pf:.2}")
+
+                break
+
+for p in P:
+    for q in Q:
+        pf_correction(p,q)
+
+print(f"iteration count = {count}")
